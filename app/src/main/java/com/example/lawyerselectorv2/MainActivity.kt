eeb41f7d2389d9d5dcc3046be58e80f590eafa9a
Fernando Parra <57759900+fparra00@users.noreply.github.com>
2022-04-22 00:02:23 +0200

    Design Login

--- app/src/main/java/com/example/lawyerselectorv2/MainActivity.kt
+++ app/src/main/java/com/example/lawyerselectorv2/MainActivity.kt
@@ -1,11 +1,27 @@
 package com.example.lawyerselectorv2
 
-import androidx.appcompat.app.AppCompatActivity
+import android.content.Intent
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import kotlinx.android.synthetic.main.activity_main.*
+
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        btnLogIn.setOnClickListener {
+            goToLoginView()
+        }
+    }
+
+    fun goToLoginView(){
+        val intent = Intent(this, LoginActivity::class.java)
+        startActivity(intent)
+    }
+
+    fun goToRegisterView(){
+
     }
 }
