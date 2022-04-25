767996218237fd6e061540786dda9ec56615960f
Fernando Parra <57759900+fparra00@users.noreply.github.com>
2022-04-25 23:38:48 +0200

    Process to Pick a Photo

--- app/src/main/java/com/example/lawyerselectorv2/SignupActivity.kt
+++ app/src/main/java/com/example/lawyerselectorv2/SignupActivity.kt
@@ -1,11 +1,54 @@
 package com.example.lawyerselectorv2
 
+import android.Manifest.permission.READ_EXTERNAL_STORAGE
+import android.app.Activity
+import android.content.Intent
+import android.content.pm.PackageManager
+import android.net.Uri
+import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.provider.MediaStore
+import android.widget.Button
+import android.widget.ImageView
+import android.widget.Toast
+import androidx.activity.result.contract.ActivityResultContract
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.core.content.ContextCompat
+import kotlinx.android.synthetic.main.activity_signup.*
+import java.nio.file.Path
+import java.util.jar.Manifest
 
 class SignupActivity : AppCompatActivity() {
+
+    companion object {
+        val IMAGE_REQUEST_CODE = 100
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_signup)
+
+        //onClicks
+        btnPhoto.setOnClickListener {
+            pickImageGallery()
+        }
     }
+
+    //Process to Pick the Photo
+    private fun pickImageGallery() {
+        val intent = Intent(Intent.ACTION_PICK)
+        intent.type = "image/*"
+        startActivityForResult(intent, IMAGE_REQUEST_CODE)
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        if (requestCode == IMAGE_REQUEST_CODE && resultCode == RESULT_OK) {
+            //Aqui guardamos la foto
+            //imagevire.setImageURI(data?.data)
+        }
+    }
+
+
 }
